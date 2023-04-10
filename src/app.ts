b544e565d4fbb142b286c63d9d4caa0cd27fb348
WillUK99 <76099444+WillUK99@users.noreply.github.com>
2023-04-10 15:43:53 +0100

    feat: create healthcheck route

--- src/app.ts
+++ src/app.ts
@@ -2,16 +2,17 @@ import express from 'express'
 import config from 'config'
 import log from './logger'
 import connect from './db/connect'
+import routes from './routes'
 
-const port = config.get('port') as number
-const host = config.get('host') as string
+const port = config.get<number>('port')
 
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-app.listen(port, host, () => {
-  log.info(`Server started at http://${host}:${port}`)
-  connect()
+app.listen(port, async () => {
+  log.info(`Server started at http://localhost:${port}`)
+  await connect()
+  routes(app)
 }) 
