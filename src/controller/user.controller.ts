ca022edddcc2ea911e60b8bf10124a35b18081e8
WillUK99 <76099444+WillUK99@users.noreply.github.com>
2023-04-16 15:04:25 +0100

    chore: create basic routes for getting users

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,14 +1,25 @@
 import { Request, Response, NextFunction } from 'express'
-import { genSaltSync, hashSync } from 'bcrypt'
 
 import User from '../model/user.model'
+import asyncHandler from '../utils/asyncHandler'
 
-export const createUser = (req: Request, res: Response, next: NextFunction) => {
+export const getUsers = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+  const users = await User.find()
+  res.json(users)
+})
+
+export const getUserById = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+  const user = await User.findById(req.params.id)
+  if (!user) {
+    return res.sendStatus(404)
+  }
+  res.json(user)
+})
+
+export const createUser = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
   const { email, password, name } = req.body
-  // const salt = genSaltSync(10)
-  // const hash = hashSync(password, salt)
   const user = new User({ email, password, name })
   user.save()
     .then((user) => res.json(user))
     .catch(next)
-}
+})
--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,10 @@ import * as UserController from './controller/user.controller'
 
 const routes = (app: Express) => {
   app.get('/healthcheck', (req: Request, res: Response) => res.sendStatus(200))
+
+  // User routes
+  app.get('/users', UserController.getUsers)
+  app.get('/users/:id', UserController.getUserById)
   app.post('/users', UserController.createUser)
 }
 
