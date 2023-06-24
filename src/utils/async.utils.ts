403cf1269eb8b69eb991fd1ac68ba5d1758a105a
WillUK99 <76099444+WillUK99@users.noreply.github.com>
2023-06-24 15:07:32 +0100

    refactor: add generics for route handlers

--- src/utils/async.utils.ts
+++ src/utils/async.utils.ts
@@ -1,7 +1,12 @@
 import { Request, Response, NextFunction } from 'express'
+import { ParamsDictionary } from 'express-serve-static-core'
+import { ParsedQs } from 'qs'
 
-type RouteHandler = (req: Request, res: Response, next: NextFunction) => Promise<any>
+type RouteHandler<P = ParamsDictionary, ResBody = any, ReqBody = any, ReqQuery = ParsedQs> =
+  (req: Request<P, ResBody, ReqBody, ReqQuery>, res: Response<ResBody>, next: NextFunction) => Promise<any>
 
-export const asyncHandler = (routeHandler: RouteHandler) => (req: Request, res: Response, next: NextFunction) => {
-  Promise.resolve(routeHandler(req, res, next)).catch(next)
-}
+export const asyncHandler = <P = ParamsDictionary, ResBody = any, ReqBody = any, ReqQuery = ParsedQs>
+  (routeHandler: RouteHandler<P, ResBody, ReqBody, ReqQuery>) =>
+  (req: Request<P, ResBody, ReqBody, ReqQuery>, res: Response<ResBody>, next: NextFunction) => {
+    Promise.resolve(routeHandler(req, res, next)).catch(next)
+  }
